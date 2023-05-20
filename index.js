c9eae8220a8867d500e2d5b020b1937e5a81d365
l-s-b <lucio_sb@yahoo.com.ar>
2023-05-20 14:40:09 -0300

    v1.2.1.5 - Still debugging Render

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const db = require('./src/db/associations');
 const {SERVER, PORT, DB_RESET} = process.env;
 const axios = require('axios');
 let RESET = JSON.parse(DB_RESET);
-console.log(RESET.force + ' (' + typeof RESET.force + ')')
 
 db.sync(RESET).then(() => {
     server.listen(PORT, async () => {
         console.log(`KCBD App on, listening on port ${PORT}`);
         //BULK CREATE
-        if (RESET.force) await axios.post(`${SERVER}/bulk`);
+        if (RESET.force) await axios.post(`${SERVER}/bulk`, null, {"timeout": 300000 });
     })
 });
