01bda47c74aeeca75adc0b30897c64bfcd44de4c
l-s-b <lucio_sb@yahoo.com.ar>
2023-05-30 02:37:39 -0300

    v1.3.0 - CORS permissions attempt #1

--- src/app.js
+++ src/app.js
@@ -7,6 +7,19 @@ const server = express();
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
+server.use(function(req, res, next) {
+  const allowedOrigins = ['http://localhost:3000', 'https://kundalinicbd.com'];
+  const requestOrigin = req.headers.origin;
+
+  if (allowedOrigins.includes(requestOrigin)) {
+    res.setHeader('Access-Control-Allow-Origin', requestOrigin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  }
+
+  next();
+})
 server.use(morgan("dev"));
 server.use(router)
 
