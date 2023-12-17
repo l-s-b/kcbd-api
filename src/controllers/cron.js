f744c8e3ba820f9a958897e06f6ad9b369a3f22a
l-s-b <lucio_sb@yahoo.com.ar>
2023-12-17 19:21:12 -0300

    v1.10.2 - Cron job fix attempt #1

--- src/controllers/cron.js
+++ src/controllers/cron.js
@@ -1,7 +1,7 @@
 const tasks = require('node-cron');
 
 const cron = () => {
-    const keepAliveCron = tasks.schedule('* */12 * * *', () => {
+    const keepAliveCron = tasks.schedule('0 10,22 * * *', () => {
         console.log('*** 12-hour keep-alive cron execution ***');
     });
 
