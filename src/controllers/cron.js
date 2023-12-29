c983c2db3394b5651b4adcb94c671f7497f3dc49
l-s-b <lucio_sb@yahoo.com.ar>
2023-12-29 15:58:25 -0300

    v1.12.0 - Back4App cron fix

--- src/controllers/cron.js
+++ src/controllers/cron.js
@@ -1,8 +1,12 @@
+// In current container service (Back4App), actual HTTP requests should be made every less than 30 minutes
+// Or else the container will hibernate. This cron aims at solving that issue.
 const tasks = require('node-cron');
+const { getProducts } = require('./products');
 
 const cron = () => {
-    const keepAliveCron = tasks.schedule('0 10,22 * * *', () => {
-        console.log('*** 12-hour keep-alive cron execution ***');
+    const keepAliveCron = tasks.schedule('*/2 * * * *', () => {
+        console.log('*** 2-minute keep-alive GET request: ***');
+        getProducts();
     });
 
     keepAliveCron.start();
