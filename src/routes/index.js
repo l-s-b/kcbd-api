5404b0fafb15f93ce872ce5e83effba78030e9f4
l-s-b <lucio_sb@yahoo.com.ar>
2024-01-14 22:44:09 -0300

    auth.5.0 - Added user signup and login routes, added checkAuth middleware in critical routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,6 +11,11 @@ const {
 const {
     postPreference
 } = require('../controllers/mp');
+const {
+    signup,
+    login
+} = require('../controllers/users');
+const { checkAuth } = require('./middleware/checkAuth');
 const router = Router();
 
 router.get("/", (req, res) => {
@@ -21,14 +26,18 @@ router.get("/cron", (req, res) => {
     res.status(200).send('Keep-alive cron')
 });
 
+// AUTH
+router.post("/signup", signup);
+router.post("/login", login);
+// PRODUCTS
 router.get("/products", getProducts);
 router.get("/products/filter", filterProducts);
 router.get("/product/:id", getProductByID);
-router.post("/product", postProduct);
-router.patch("/product/:id?", patchProduct);
-router.delete("/product/:id?", deleteProduct);
-router.post("/bulk", postBulk);
-
+router.post("/product", checkAuth, postProduct);
+router.patch("/product/:id?", checkAuth, patchProduct);
+router.delete("/product/:id?", checkAuth, deleteProduct);
+router.post("/bulk", checkAuth, postBulk);
+// MP CHECKOUT
 router.post("/create_preference", postPreference);
 
 module.exports = router;
