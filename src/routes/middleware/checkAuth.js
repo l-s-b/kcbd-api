7e489ffc21b616ec1718cdde6dc7a42645151262
l-s-b <lucio_sb@yahoo.com.ar>
2024-01-16 04:43:47 -0300

    auth.5.1 - checkAuth tweaks, ready to test in prod

--- src/routes/middleware/checkAuth.js
+++ src/routes/middleware/checkAuth.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { AUTH_SALT } = process.env;
 
 const checkAuth = (req, res, next) => {
-  const token = req.header('Authorization').split(" ")[1];
+  const token = req.header('Authorization')?.split(" ")[1];
   if (!token) {
     return res.status(401).json(
       { message: 'Unauthorized: No token provided' }
@@ -20,7 +20,8 @@ const checkAuth = (req, res, next) => {
 
     // Attach decoded user info to req object for further use
     req.user = decoded;
-    next();
+    const response = res.status(307).json('Auth checked!')
+    next() || response;
   });
 };
 
